eb71fb153655983ff79074b322285f9d5aec9216
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2024-05-25 07:28:00 +0200

    Remove placeholders

--- docs/roboter/mbot.md
+++ docs/roboter/mbot.md
@@ -3,3 +3,5 @@ sidebar_position: 2
 ---
 
 # mBot 2
+
+TODO
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
-import { Link } from 'react-router-dom';
 
 const FeatureList = [
   {
